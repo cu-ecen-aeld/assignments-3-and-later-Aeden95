29f363f71c4fafb846ceada1a9171623cab54a68
Aeden95 <andrea.cosci4@gmail.com>
2024-05-26 06:03:51 -0400

    Added threading function

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,44 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to lock mutex\n");
+        return NULL;
+    }
+    DEBUG_LOG("Mutex locked\n");
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to unlock mutex\n");
+        return NULL;
+    }
+    DEBUG_LOG("Mutex unlocked\n");
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data* tdata = (struct thread_data*)malloc(sizeof(struct thread_data));
+    if (tdata == NULL) {
+        ERROR_LOG("Failed to allocate memory for thread_data\n");
+        return false;
+    }
+
+    tdata->mutex = mutex;
+    tdata->wait_to_obtain_ms = wait_to_obtain_ms;
+    tdata->wait_to_release_ms = wait_to_release_ms;
+    tdata->thread_complete_success = false; 
+
+    if (pthread_create(thread, NULL, threadfunc, (void*)tdata) != 0) {
+        ERROR_LOG("Failed to create thread\n");
+        free(tdata);
+        return false;
+    }
+
+    return true;
 }
 
